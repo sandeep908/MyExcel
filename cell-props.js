3409cabf3edcc0bc477b30452c50b0e193ad6b60
Sandeep Kumar Singh <sandeep908singh908@gmail.com>
2023-09-20 05:00:39 +0000

    create new file

---
+++ cell-props.js
@@ -0,0 +1,19 @@
+let sheetDB = [] ;
+
+for(let i=0 ;i<rows ;i++){
+    let sheetRow= [] ;
+    for(let j=0 ;j<cols ;j++){
+        let cellProp ={
+            bold:false ,
+            italic:false ,
+            underline:false ,
+            alignment:"left" ,
+            fontFamily:"Times New Romen" ,
+            fontSize:"14" ,
+            fontColor:"#000000" ,
+            BGColor:"#000000"
+        }
+        sheetRow.push(cellProp) ;
+    }
+    sheetDB.push(sheetRow) ;
+}
