30d0f21b698509987da1e15e601d8680ffd8063f
Sandeep Kumar Singh <sandeep908singh908@gmail.com>
2023-09-20 14:59:22 +0530

    Update cell-props.js

--- cell-props.js
+++ cell-props.js
@@ -17,3 +17,43 @@ for(let i=0 ;i<rows ;i++){
     }
     sheetDB.push(sheetRow) ;
 }
+
+let bold = document.querySelector(".bold");
+let italic = document.querySelector(".italic");
+let underline = document.querySelector(".underline");
+let fontSize = document.querySelector(".font-size-prop");
+let fontFamily = document.querySelector(".font-family-prop");
+let fontColor = document.querySelector(".font-color-prop");
+let BGColor = document.querySelector(".BGcolor-prop");
+let alignment = document.querySelectorAll(".alignment");
+let leftAlign = alignment[0] ;
+let centerAlign = alignment[1] ;
+let rightAlign = alignment[2] ;
+
+let activeColorProp = "#024566" ;
+let inactiveColorProp = "#f3454g" ;
+
+// Application of two way binding
+// Attach property listeners on each event
+bold.addEventListener("click" ,(e)=>{
+    let address = addressBar.value ;
+    let [cell ,cellProp] = activecell(address) ;
+    // modification
+    cellProp.bold = !cellProp.bold ; //negate the present value
+    cell.style.fontWeight = cellProp.bold ? "bold" : "normal" ; //UI change occured
+    bold.style.backgroundColor = cellProp.bold ? activeColorProp :inactiveColorProp ;
+})
+
+function activecell(address){
+    let [rid ,cid] = decodeRIDCIDfromAddress(address) ;
+    // access cell and storage object
+    let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`) ;
+    let cellProp = sheetDB[rid][cid] ;
+    return [cell ,cellProp] ;
+}
+
+function decodeRIDCIDfromAddress(address){
+    let rid = Number(address.slice(1)-1) ;
+    let cid = Number(address.charCodeAt(0))-65 ;
+    return [rid ,cid] ;
+}
